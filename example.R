b1bfe820e55a323f8391242ccd9b9daa3cba832c
Oscar Kjell <oscarkjell@Oscars-MacBook-Pro.local>
2024-05-06 19:46:51 +0200

    update example including call to Matrix::colSums

--- example.R
+++ example.R
@@ -16,9 +16,9 @@ dtm <- ldaDtm(data = data,
               stopwords = stopwords::stopwords("en", source = "snowball"))
 
 # Checking the results from the dtm
-length(colSums(dtm$train_dtm))
-colSums(dtm$train_dtm)[1:20]
-colSums(dtm$train_dtm)[(length(colSums(dtm$train_dtm)) - 100):length(colSums(dtm$train_dtm))]
+length(Matrix::colSums(dtm$train_dtm))
+Matrix::colSums(dtm$train_dtm)[1:20]
+Matrix::colSums(dtm$train_dtm)[(length(Matrix::colSums(dtm$train_dtm)) - 100):length(Matrix::colSums(dtm$train_dtm))]
 
 
 model <- ldaModel(dtm = dtm,
