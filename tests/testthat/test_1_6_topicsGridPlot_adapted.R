79ed24178da3fb77557ed5e325c3c5c680d200f2
moomoofarm1 <46774289+moomoofarm1@users.noreply.github.com>
2024-07-03 23:56:18 +0200

    Update test_1_6_topicsGridPlot_adapted.R

--- tests/testthat/test_1_6_topicsGridPlot_adapted.R
+++ tests/testthat/test_1_6_topicsGridPlot_adapted.R
@@ -16,50 +16,52 @@ preds = topicsPreds(
 
 
 tests <- topicsTest(
-  model=model,
+  model = model,
   preds = preds,
   data =  Language_based_assessment_data_3_100,
   pred_var_x = 'hilstotal',
-  pred_var_y = 'swlstotal'
+  pred_var_y = NULL#'swlstotal'
 )
 
-tests <- topicsTest(
-  model=model,
-  preds = preds,
-  data =  Language_based_assessment_data_3_100,
-  pred_var_x = 'hilstotal',
-  pred_var_y = NULL
-)
+# tests <- topicsTest(
+#   model=model,
+#   preds = preds,
+#   data =  Language_based_assessment_data_3_100,
+#   pred_var_x = 'hilstotal',
+#   pred_var_y = NULL
+# )
 
-topicsPlot(model = model,
-           test = tests,
-           dim = 1
-           ,seed = 42)
-topicsPlot(model = model,
-                 test = tests,
-                 dim = 2
-                 ,seed = 42)
-topicsPlot(model = model,
-           test = tests,
-           dim = 3
-           ,seed = 42)
+# topicsPlot(model = model,
+#            test = tests,
+#            dim = 1
+#            ,seed = 42)
+# topicsPlot(model = model,
+#                  test = tests,
+#                  dim = 2
+#                  ,seed = 42)
+# topicsPlot(model = model,
+#            test = tests,
+#            dim = 3
+#            ,seed = 42)
 
-tests <- topicsTest(
-  model=model,
-  preds = preds,
-  data =  Language_based_assessment_data_3_100,
-  pred_var_x = 'hilstotal',
-  pred_var_y = 'swlstotal'
-)
+# topicsPlot(model = model,
+#            test = tests,
+#            grid = TRUE,
+#            dim = 1
+#            ,seed = 42)
 topicsPlot(model = model,
            test = tests,
-           dim = 1
-           ,seed = 42)
-topicsPlot(model = model,
-           test = tests,
-           dim = 2
-           ,seed = 42)
-topicsPlot(model = model,
-           test = tests,
-           dim = 3
-           ,seed = 42)
+           grid = FALSE,
+           p_threshold = 0.99,
+           #dim = 1,
+           seed = 42)
+
+# topicsPlot(model = model,
+#            test = tests,
+#            dim = 3
+#            ,seed = 42)
+
+# topicsPlot(model = model,
+#            test = tests,
+#            grid = FALSE,
+#            ,seed = 42)
