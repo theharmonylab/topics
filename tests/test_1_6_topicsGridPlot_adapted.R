fe10e2e5e5bdbcafa428e28a7918d8e67b6d14a6
moomoofarm1 <46774289+moomoofarm1@users.noreply.github.com>
2024-07-19 11:38:56 +0200

    Add files via upload
    For testing grid plots.

---
+++ tests/test_1_6_topicsGridPlot_adapted.R
@@ -0,0 +1,60 @@
+# TODO: Put the dot plot function in the main to plot every category. Just some ggplot.
+
+rm(list=ls())
+gc()
+
+library(text)
+library(topics)
+dtmtest <- topicsDtm(
+  data = Language_based_assessment_data_3_100$harmonywords
+)
+
+model = topicsModel(dtmtest,num_topics = 50)
+
+preds = topicsPreds(
+  model = model, 
+  data = Language_based_assessment_data_3_100$harmonywords
+)
+
+
+tests <- topicsTest(
+  model = model,
+  preds = preds,
+  data =  Language_based_assessment_data_3_100,
+  pred_var_x = 'hilstotal',
+  pred_var_y = 'swlstotal'
+)
+
+topicsPlot(model = model,
+           test = tests,
+           grid_plot = TRUE,
+           p_threshold = 0.99,
+           dim = 1
+           ,seed = 42)
+
+topicsPlot(model = model,
+           test = tests,
+           grid_plot = TRUE,
+           p_threshold = 0.99,
+           dim = 2
+           ,seed = 42)
+
+topicsPlot(model = model,
+           test = tests,
+           grid_plot = TRUE,
+           dim = 3
+           ,seed = 42)
+
+tests <- topicsTest(
+  model=model,
+  preds = preds,
+  data =  Language_based_assessment_data_3_100,
+  pred_var_x = 'hilstotal',
+  pred_var_y = NULL
+)
+
+topicsPlot(model = model,
+           test = tests,
+           grid_plot = FALSE,
+           p_threshold = 0.99,
+           seed = 42)
