43a9da94a358a0af776b9758c31b6237cf23706e
moomoofarm1 <46774289+moomoofarm1@users.noreply.github.com>
2024-07-23 11:20:13 +0200

    Update test_1_6_topicsGridPlot_adapted.R
    update to support manual setting of topicsScatterLegend.

--- tests/test_1_6_topicsGridPlot_adapted.R
+++ tests/test_1_6_topicsGridPlot_adapted.R
@@ -1,3 +1,7 @@
+# remove.packages("topics")
+# devtools::document()
+# devtools::build()
+
 rm(list=ls())
 gc()
 
@@ -9,7 +13,7 @@ dtmtest <- topicsDtm(
 
 model <- topicsModel(dtmtest)
 
-preds = topicsPreds(
+preds <- topicsPreds(
   model = model, 
   data = topics::data$harmonywords
 )
@@ -20,9 +24,24 @@ tests <- topicsTest(
   preds = preds,
   data =  topics::data,
   pred_var_x = 'hilstotal',
+  pred_var_y = 'swlstotal'
+)
+
+tests <- topicsTest(
+  model = model,
+  preds = preds,
+  data =  topics::data,
+  pred_var_x = 'hilstotal',
   pred_var_y = 'swlstotal',
   control_vars = c('age','gender')
 )
+
+
+topicsPlot(model = model,
+           test = tests,
+           grid_plot = FALSE,
+           p_threshold = 0.99
+           ,seed = 42)
 
 topicsPlot(model = model,
            test = tests,
@@ -37,6 +56,22 @@ topicsPlot(model = model,
            p_threshold = 0.99,
            dim = 2
            ,seed = 42)
+
+topicsPlot(model = model,
+           test = tests,
+           grid_plot = TRUE,
+           p_threshold = 0.99,
+           dim = 2,
+           way_popout_topics = 'max_y',
+           seed = 42)
+
+topicsPlot(model = model,
+           test = tests,
+           grid_plot = TRUE,
+           p_threshold = 0.99,
+           dim = 2,
+           user_spec_topics = c('t_1', 't_20'),
+           seed = 42)
 
 topicsPlot(model = model,
            test = tests,
