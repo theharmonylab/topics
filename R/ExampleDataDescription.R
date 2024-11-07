f70a988ce6ec11b3917e10ec71b1191097e66619
LeonAckermann <leonmax.ackermann@gmail.com>
2024-11-07 18:57:35 +0100

    new features for topicsGrams

--- R/ExampleDataDescription.R
+++ R/ExampleDataDescription.R
@@ -1,18 +1,32 @@
-#' Text and numeric data for 10 participants.
-#'
-#' The dataset is a shortened version of the data sets of Study 3-5
-#' from Kjell et al., (2018; https://psyarxiv.com/er6t7/).
-#'
+# #' Text and numeric data for 10 participants.
+# #'
+# #' The dataset is a shortened version of the data sets of Study 3-5
+# #' from Kjell et al., (2018; https://psyarxiv.com/er6t7/).
+# #'
+# #' @format A data frame with 40 participants and 8 variables:
+# #' \describe{
+# #'   \item{harmonywords}{descriptive words where respondents describe their harmony in life}
+# #'   \item{satisfactionwords}{descriptive words where respondents describe their satisfaction with life}
+# #'   \item{harmonytexts}{text where respondents describe their harmony in life}
+# #'   \item{satisfactiontexts}{text where respondents describe their satisfaction with life}
+# #'   \item{hilstotal}{total score of the Harmony In Life Scale}
+# #'   \item{swlstotal}{total score of the Satisfaction With Life Scale}
+# #'   \item{age}{respondents age in years}
+# #'   \item{gender}{respondents gender 1=male, 2=female}
+# #' }
+# #' @source \url{https://osf.io/preprints/psyarxiv/er6t7/}
+# "Language_based_assessment_data_8"
+
 #' @format A data frame with 40 participants and 8 variables:
 #' \describe{
-#'   \item{harmonywords}{descriptive words where respondents describe their harmony in life}
-#'   \item{satisfactionwords}{descriptive words where respondents describe their satisfaction with life}
-#'   \item{harmonytexts}{text where respondents describe their harmony in life}
-#'   \item{satisfactiontexts}{text where respondents describe their satisfaction with life}
-#'   \item{hilstotal}{total score of the Harmony In Life Scale}
-#'   \item{swlstotal}{total score of the Satisfaction With Life Scale}
-#'   \item{age}{respondents age in years}
-#'   \item{gender}{respondents gender 1=male, 2=female}
+#'   \item{Depphrase}{phrases where respondents describe their experience with depression in life}
+#'   \item{Worphrase}{phrases where respondents describe their experience with anxiety in life}
+#'   \item{Deptext}{text where respondents describe their experience with depression in life}
+#'   \item{Wortext}{text where respondents describe their experience with anxiety in life}
+#'   \item{Gender}{respondents gender 0=male, 1=female}
+#'   \item{Age}{respondents age in years}
+#'   \item{PHQ9tot}{total score of the respondents PHQ-9}
+#'   \item{GAD7tot}{total score of the respondents GAD-7}
 #' }
-#' @source \url{https://osf.io/preprints/psyarxiv/er6t7/}
-"Language_based_assessment_data_8"
+"response_format_500_data_anonymized"
+
