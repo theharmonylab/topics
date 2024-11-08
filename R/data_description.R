66d1a628656c7ab5f0c849f09412a07b0e7d527d
Oscar Kjell <oscarkjell@Oscars-MacBook-Pro-2.local>
2024-11-08 17:02:48 +0100

    Fixing example data warning.

--- R/data_description.R
+++ R/data_description.R
@@ -1,22 +1,6 @@
-# #' Text and numeric data for 10 participants.
-# #'
-# #' The dataset is a shortened version of the data sets of Study 3-5
-# #' from Kjell et al., (2018; https://psyarxiv.com/er6t7/).
-# #'
-# #' @format A data frame with 40 participants and 8 variables:
-# #' \describe{
-# #'   \item{harmonywords}{descriptive words where respondents describe their harmony in life}
-# #'   \item{satisfactionwords}{descriptive words where respondents describe their satisfaction with life}
-# #'   \item{harmonytexts}{text where respondents describe their harmony in life}
-# #'   \item{satisfactiontexts}{text where respondents describe their satisfaction with life}
-# #'   \item{hilstotal}{total score of the Harmony In Life Scale}
-# #'   \item{swlstotal}{total score of the Satisfaction With Life Scale}
-# #'   \item{age}{respondents age in years}
-# #'   \item{gender}{respondents gender 1=male, 2=female}
-# #' }
-# #' @source \url{https://osf.io/preprints/psyarxiv/er6t7/}
-# "Language_based_assessment_data_8"
-
+#' Example data about mental health descirptions
+#'.
+#'
 #' @format A data frame with 500 participants and 13 variables:
 #' \describe{
 #'   \item{Depselect}{Words that respondents have selected from a pre-defined list}
@@ -32,5 +16,5 @@
 #'   \item{PHQ9tot}{total score of the respondents PHQ-9}
 #'   \item{GAD7tot}{total score of the respondents GAD-7}
 #' }
+#' @source <https://osf.io/preprints/psyarxiv/p67db>
 "dep_wor_data"
-
