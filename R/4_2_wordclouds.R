7bc45438b19e2b50697fb9407fa03343c4d99eab
moomoofarm1 <46774289+moomoofarm1@users.noreply.github.com>
2025-03-13 14:30:55 +0100

    Update 4_2_wordclouds.R

--- R/4_2_wordclouds.R
+++ R/4_2_wordclouds.R
@@ -402,7 +402,7 @@ create_plots <- function(
                  dplyr::mutate(label_content = if_else(source == "negDic",
                                                 sprintf("<b><i>%s</i></b>", Word),
                                                 Word))
-               target_topic$phi_adj <- target_topic$phi^3 / sum(target_topic$phi^3)
+               target_topic$phi_adj <- target_topic$phi^5 / sum(target_topic$phi^5) # Make the differences of size and color more obvious.
            }else{stop('Invalid settings for the parameter "highlight_topic_words".\nConsider use the default option!\n')}
         }else{target_topic <- df_list[[as.numeric(sub(".*_", "", i))]]}
         
